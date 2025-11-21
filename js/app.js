add53c7ee73ed1ef2730bcbb0c1758515e78c5d9
guillaume-smbg <guillaume.kettenmeyer@smbg-conseil.fr>
2025-11-21 19:40:45 +0100

    Update app.js

--- js/app.js
+++ js/app.js
@@ -15,7 +15,7 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19
 }).addTo(map);
 
-map.setView([46.8, 2.4], 6);
+map.setView([46.8, 2.4], 6, { animate: false });
 
 
 /* ============================================================
